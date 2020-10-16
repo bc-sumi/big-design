c651709da809af4d4dee06254babdbf6c8004269
Daniel Almaguer <daniel.almaguer@bigcommerce.com>
2020-10-16 11:49:39 -0500

    8444aaa9

---
+++ _next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {"/Modal/ModalPage":["static\u002Fchunks\u002Fpages\u002FModal\u002FModalPage.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
